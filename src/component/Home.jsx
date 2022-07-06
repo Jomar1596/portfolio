00f67f4330587ff806f741600740e71534802c1f
Jomar Perante <jomarperante@gmail.com>
2022-07-06 11:50:36 +0800

    front-end

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
             <div className="max-w-[850px] mx-auto px-8  flex flex-col justify-center h-full">
                 <p className="">Hi, my name is</p>
                 <h1 className="text-8xl md:text-6xl sm:text-5xl text-blue-600">Jomar Perante</h1>
-                <h1 className="text-6xl md:text-5xl sm:text-4xl">A Fron-End Web Developer</h1>
+                <h1 className="text-6xl md:text-5xl sm:text-4xl">A Front-End Web Developer</h1>
                 <p className="max-w-[600px] md:max-w-[450px] sm:max-w-[350px] mt-[0.5rem]">
                     As a Front-End Web Developer my goal is to give my Clients an Exceptional
                     experience thru Responsive Web Applications. Always welcome for new opportunities and learnings.
